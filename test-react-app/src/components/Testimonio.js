d8ade2b22e211bca063b2d3bd5073ff6fc364a70
Manuel Gago <manuelmeg96@gmail.com>
2023-07-10 16:12:59 +0000

    1er componente

---
+++ test-react-app/src/components/Testimonio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Testimonio() {
+  return (
+  <div className='contenedor-testimonio'>
+    <img 
+      className='imagen-testimonio'
+      src={require('../images/Emma-fcc.png')}
+      alt='Imagen de Emma'
+    />
+  </div>
+  );
+}
